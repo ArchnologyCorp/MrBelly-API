2b0441b9c6a5e46b431cffcc1234a7a1ab245e21
LeoDev <leonardoiigd2013@gmail.com>
2022-08-11 21:28:38 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# from flask import Flask, request
+# from flask_cors import CORS, cross_origin 
+import json
+
+devs = ['Carlos', 'Teylor', 'Cleber', 'Leonardo']
+print('Devs: {}'.format(json.dumps(devs)))
+print('Hello Word!')
