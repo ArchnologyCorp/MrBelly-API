f0e7f3840c31e2bc3116c1d37e353b977a0aaa47
LeoDev <leonardoiigd2013@gmail.com>
2022-08-11 21:43:36 -0300

    deploy api

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# from flask import Flask, request
+from flask import Flask
 # from flask_cors import CORS, cross_origin 
 import json
 
-devs = ['Carlos', 'Teylor', 'Cleber', 'Leonardo']
-print('Devs: {}'.format(json.dumps(devs)))
-print('Hello Word!')
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    devs = ['Carlos', 'Teylor', 'Cleber', 'Leonardo']
+    print('Devs: {}'.format(json.dumps(devs)))
+    print('Hello Word!')
+    return 'Hello World!'
