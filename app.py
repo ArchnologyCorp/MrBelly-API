b97abc17e8e49832b59086197204085605cbe946
CarllosDc13 <carlos.rtdc@gmail.com>
2022-08-24 22:23:57 -0300

    Debits
    CRUD Completed

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-from flask import Flask
-# from flask_cors import CORS, cross_origin 
+from flask import Flask, request
+# from flask_cors import CORS, cross_origin
 import json
 import db
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def hello():
     devs = ['Carlos', 'Teylor', 'Cleber', 'Leonardo', 'Railson']
     print('Devs: {}'.format(json.dumps(devs)))
     print('Hello Word!')
     return devs
 
-@app.route('/debits', methods = ['GET'])
-def getDebits():
-    return db.getDebits()
-       
+@app.route('/debits', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def debitsEndPoint():
+    if request.method == 'GET':
+        return db.getDebits()
+    elif request.method == 'POST':
+        debit = request.get_json()
+        return db.postDebit(debit)
+    elif request.method == 'DELETE':
+        debit = request.get_json()
+        response = db.deleteDebit(debit['id'])
+        return {'success':  response}
+    elif request.method == 'PATCH':
+        debit = request.get_json()
+        return db.getDebit(debit['id'])
+    elif request.method == 'PUT':
+        debit = request.get_json()
+        # _debit['id'] = debit or _debit['id']
+        response = db.putDebit(debit)
+        return {'success': response}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- db.py
+++ db.py
@@ -24,6 +24,59 @@ def getDebits():
         print(error)
     return response
 
+def getDebit(id):
+    response = {}
+    try:
+        conn = openConnection() 
+        cur = conn.cursor()
+        cur.execute(f'SELECT * FROM tb_cobranca WHERE id = {int(id)}')
+        data = cur.fetchall()
+        response = buildJson({'id': 0, 'creation_date': None, 'updated_at': None, 'description': '', 'id_user': 0}, data)[0]
+            
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return response
+
+def postDebit(entity):
+    response = {}
+    try:
+        conn = openConnection() 
+        cur = conn.cursor()
+        cur.execute(f'INSERT INTO tb_cobranca (descricao, id_usuario) VALUES(%s,%s) RETURNING id', (entity["description"], entity["id_user"]))
+        conn.commit()
+        entity['id'] = int(cur.fetchone()[0])
+        response = entity
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return response
+
+def putDebit(entity):
+    response = False
+    try:
+        conn = openConnection() 
+        cur = conn.cursor()
+        cur.execute(f'UPDATE tb_cobranca SET descricao = %s, id_usuario = %s, data_atualizacao = now() WHERE id = %s', (entity["description"], entity["id_user"], entity["id"]))
+        conn.commit()
+        response = True
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return response
+
+def deleteDebit(id):
+    response = False
+    try:
+        conn = openConnection() 
+        cur = conn.cursor()
+        cur.execute(f'DELETE FROM tb_cobranca WHERE id = {int(id)}')
+        conn.commit()
+        response = True
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return response
+
+
 def buildJson(obj, values):
     array = []
     for value in values:
