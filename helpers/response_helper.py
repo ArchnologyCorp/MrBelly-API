05662d946c147c5ed58c8f994ae31ac4e058885b
LeoDev <leonardoiigd2013@gmail.com>
2022-09-24 23:54:44 -0300

    fix error response field

--- helpers/response_helper.py
+++ helpers/response_helper.py
@@ -4,4 +4,4 @@ def sucessResponse(data = {}, msg = 'Função realizada com sucesso'):
     return ({'success': True, 'msg': msg, 'data': data})
 
 def errorResponse(msg, statusCode = 400):
-    return {'sucess': False, 'msg': msg}, statusCode
+    return {'success': False, 'msg': msg}, statusCode
