e8d82e08a47c5874008e6e28959681807e95fcc3
LeoDev <leonardoiigd2013@gmail.com>
2023-03-30 20:19:21 -0300

    re add procfile

--- repository/db.py
+++ repository/db.py
@@ -2,8 +2,8 @@ import psycopg2
 
 def openConnection():
     conn = psycopg2.connect(
-        host="ec2-3-223-242-224.compute-1.amazonaws.com", 
-        database="dcusk4ojn43g3h",
-        user="txtfgbvmfpcowk",
-        password="a1696082d98712652af6daeafb7eae810e09817056ce48f37b5a39687ca6689c")
+        host="bbio43cxsc7culcqqqnp-postgresql.services.clever-cloud.com", 
+        database="bbio43cxsc7culcqqqnp",
+        user="ugfhyj2ph23mx4h4rohn",
+        password="J7685ISIeik3iWJTkEJOxqK1wqVdqK")
     return conn
