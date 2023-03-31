30f3eaa7dacc8819696c980980086d453406a3a6
LeoDev <leonardoiigd2013@gmail.com>
2023-03-30 21:16:21 -0300

    update value_helper.py

--- helpers/value_helper.py
+++ helpers/value_helper.py
@@ -2,10 +2,9 @@ from datetime import datetime
 from decimal import Decimal
 
 def convertValue(value):
-    match value:
-        case float() | Decimal():
-            return float(value)
-        case datetime():
-            return value.isoformat()
-        case _:
-            return value
+    if value == float() | Decimal(): 
+        return float(value)
+    elif value == datetime():
+        return value.isoformat()
+    else:
+        return value
